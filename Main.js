30e926a422a68f236d18cc60593d776196ac06e8
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-05-24 11:32:37 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,52 +1,53 @@
 const DiscordJS = require('discord.js');
 
 const {
-	Rcon
+  Rcon
 } = require('rcon-client');
+
 const TextFormat = require('chalk');
 
 const {
-	TOKEN
+  TOKEN
 } = process.env;
 
 const {
-	PREFIX
+  PREFIX
 } = process.env;
 
 const Host = process.env.IP;
 
 const {
-	Port
+  Port
 } = process.env;
 
 const {
-	Password
+  Password
 } = process.env;
 
 const Client = new DiscordJS.Client({
-	disableEveryone: true
+  disableEveryone: true
 });
 
-Client.on("ready", async() => {
-	console.log(TextFormat.BLUE("Rcon Bot") + TextFormat.GREEN("Is Now Online!"));
+Client.on("ready", () => {
+  console.log("Rcon Bot Is Now Online!");
 });
 
 Client.on("message", async msg => {
-	const rcon = await Rcon.connect({
-		host: Host,
-		port: Port,
-		password: Password
-	});
+  const rcon = await Rcon.connect({
+    host: Host,
+    port: Port,
+    password: Password
+  });
 
-	const args = msg.content.slice(Prefix.length).trim().split(/ +/g);
+  const args = msg.content.slice(PREFIX.length).trim().split(/ +/g);
 
-	if (msg.content.startsWith(Prefix)) {
-		let responses = await Promise.all([
-			rcon.send(args.join(""))
-		]);
-		msg.channel.send("The command has been sent to the Minecraft Server. Check out the console to see if it worked. If you got any RCON errors. Just reload/restart the Minecraft Server.");
-		rcon.end();
-	}
+  if (msg.content.startsWith(PREFIX)) {
+    let responses = await Promise.all([
+      rcon.send(args.join(""))
+    ]);
+    msg.channel.send("The command has been sent to the Minecraft Server. Check out the console to see if it worked. If you got any RCON errors. Just reload/restart the Minecraft Server.");
+    rcon.end();
+  }
 });
 
-Client.login(TOKEN)
+Client.login(TOKEN);
